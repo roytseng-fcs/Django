359d90d1807ba9d5066c8fbfb133731b062ae8a7
Roy Tseng <roytseng@floatingcube.com>
2017-04-21 01:22:44 +0700

    First django website

--- django_is_good/blog/models.py
+++ django_is_good/blog/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+   title = models.CharField(max_length=500) 
+   slug = models.SlugField()
+   body = models.TextField()
+   create_date = models.DateTimeField('date published')
+   update_date = models.DateTimeField('date created')
